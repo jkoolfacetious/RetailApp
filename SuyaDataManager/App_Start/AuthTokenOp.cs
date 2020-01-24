4eb985096d198f9acdd3da7300eb2e3f73e4df4d
jesse <jesse.o.dickson@leidos.com>
2020-01-23 21:41:37 -0600

    Adds Swagger with Authentication
    Adds swagger with parameters

---
+++ SuyaDataManager/App_Start/AuthTokenOp.cs
@@ -0,0 +1,56 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace SuyaDataManager.App_Start
+{
+    public class AuthTokenOp : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            //getting the path and it has a post command????
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                //its a post command/ operation hitting the api
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    //its going to pass these into the body of post command
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+}
