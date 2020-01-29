2f542e4aecbd15f4fa116275a676541061453cfb
jesse <jesse.o.dickson@leidos.com>
2020-01-28 20:30:58 -0600

    Set Up MVVM
    Added MVVM Folders

---
+++ FCIAUserInterFace/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using FCIAUserInterFace.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FCIAUserInterFace
+{
+   public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //on start up launch the shell view model 
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+
+
+}
