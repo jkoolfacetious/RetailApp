6827bf37b548924a72f447ecdef6e16f273ebe4e
jesse <jesse.o.dickson@leidos.com>
2020-02-13 23:24:20 -0600

    Add User Table
    adds user table to the database

--- FCIAUserInterFace/Bootstrapper.cs
+++ FCIAUserInterFace/Bootstrapper.cs
@@ -11,15 +11,43 @@ namespace FCIAUserInterFace
 {
    public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             //on start up launch the shell view model 
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 
